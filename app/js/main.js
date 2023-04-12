575d39045ffc6b84a7dc7c9d1ddde27499771e40
TimWolfskin <timwolfskin013@gmail.com>
2023-04-12 08:36:39 +0300

    delete gulp

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,7 @@
-import Swiper, { Navigation, Pagination } from "swiper";
 
-
-Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper(".swiper", {
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
   },
 });
